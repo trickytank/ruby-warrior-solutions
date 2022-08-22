b85a1f034307b7a73de77a2191aa49a454d81294
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-22 21:17:53 +1000

    Added solution to level 9

---
+++ player_level_09.rb
@@ -0,0 +1,54 @@
+class Player
+  def initialize()
+    @health = 20
+    @direction = 'forward'
+  end
+  def play_turn(warrior)
+    # add your code here
+    if warrior.feel.wall? then
+        warrior.pivot!
+    else
+        if @direction == 'forward' then
+            if warrior.feel.empty? then
+                ahead = warrior.look
+                if warrior.look[1].enemy? || warrior.look[2].enemy? then
+                    if warrior.health < @health then
+                        warrior.walk!
+                    else
+                        warrior.shoot!
+                    end
+                else
+                    if warrior.health < @health
+                        if warrior.health < 7 then
+                            warrior.walk!:backward
+                            @direction = 'backward'
+                        else
+                            warrior.walk!
+                        end
+                    elsif warrior.health < 20 then
+                        warrior.rest!
+                    else
+                        warrior.walk!
+                    end
+                end
+            else 
+                if warrior.feel.captive?
+                    warrior.rescue!
+                else
+                    warrior.attack!
+                end
+            end
+        else
+            if warrior.feel(:backward).captive? then
+                warrior.rescue!:backward
+            elsif warrior.feel(:backward).wall? then
+                @direction = 'forward'
+                warrior.rest!
+            else
+                warrior.walk!:backward
+            end
+        end
+    end
+    @health = warrior.health
+  end
+end
